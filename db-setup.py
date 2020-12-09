49f7c830179987ec99c232e311e44939bd4ab1a1
Wynn <wynn.blevins@apivista.com>
2020-12-09 13:14:34 -0500

    Initial add of chatter table

--- db-setup.py
+++ db-setup.py
@@ -3,17 +3,20 @@ import time
 import sys 
 import ConfigParser
 
+config = ConfigParser.RawConfigParser()
+config.read('db.properties')
+
 def startDockerContainer(): 
     # create a docker container
-    os.system('docker run -p 5432:5432 --name personalpostgres -e POSTGRES_PASSWORD=postgres -d postgres:9.6')
+    os.system('docker run -p 5432:5432 --name ' + config.get("database", "CONTAINER_NAME") + ' -e POSTGRES_PASSWORD=postgres -d postgres:9.6')
     time.sleep(10)
 
 def createDatabase():
-    config = ConfigParser.RawConfigParser()
-    config.read('db.properties')
-
-    os.system("docker exec -it personalpostgres createdb -U postgres -h localhost -w predictor_db")
-    os.system('liquibase --classpath=' + config.get("database", "CLASSPATH") + ' --changeLogFile="./db/db.changelog-master.xml" --driver="org.postgresql.Driver" --url="jdbc:postgresql://10.0.0.228:5432/predictor_db" --username=postgres --password=postgres update')
+    command = 'liquibase --classpath=' + config.get("database", "CLASSPATH") + ' --changeLogFile="./db/db.changelog-master.xml" --driver="org.postgresql.Driver" --url="jdbc:postgresql://' + config.get("database", "IP") + ':5432/' + config.get("database", "DATABASE") + '" --username=' + config.get("database", "USERNAME") + ' --password=' + config.get("database", "PASSWORD") + ' update'
+    print(command)
+    
+    os.system("docker exec -it " + config.get("database", "CONTAINER_NAME") + " createdb -U postgres -h localhost -w " + config.get("database", "DATABASE"))
+    os.system('liquibase --classpath=' + config.get("database", "CLASSPATH") + ' --changeLogFile="./db/db.changelog-master.xml" --driver="org.postgresql.Driver" --url="jdbc:postgresql://10.0.0.228:5432/' + config.get("database", "DATABASE") + '" --username=' + config.get("database", "USERNAME") + ' --password=' + config.get("database", "PASSWORD") + ' update')
 
 def pgup():
     print("Starting...")
@@ -22,15 +25,15 @@ def pgup():
 
 def pgdown(): 
     print("Stopping...")
-    os.system('docker stop personalpostgres')
+    os.system('docker stop ' + config.get("database", "CONTAINER_NAME"))
 
 def pgnuke(): 
     print("Removing...")
-    os.system('docker rm personalpostgres -f')
+    os.system('docker rm ' + config.get("database", "CONTAINER_NAME") + ' -f')
 
 def pgrestart(): 
     print("Restarting...")
-    os.system('docker restart personalpostgres')
+    os.system('docker restart ' + config.get("database", "CONTAINER_NAME"))
     
 def pgupdate():
     scaffoldDatabases()
